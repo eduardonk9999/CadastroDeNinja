9cccd490d0917f2b0e6ea976455efe5c9c0260c2
eduardonk9999 <thiago.novak7@gmail.com>
2025-01-22 20:17:24 -0300

    columns orm

--- src/main/java/dev/duzera/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/dev/duzera/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -3,8 +3,10 @@ package dev.duzera.CadastroDeNinjas.Missoes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-//AQUI É O EXEMPLO DO ORM SENDO USADO COM O JPA
+// AQUI É O EXEMPLO DO ORM SENDO USADO COM O JPA
 // METODOS NO BANCO DE ADOS NOS CHAMAMOS DE QUERYS
 // ORM TRADUZ O QUE TÁ NA NOSSA CLASS PARA O BANCO DE DADOS
+
+
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
--- src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -15,10 +15,21 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "idade")
     private int idade;
+
+    @Column(name = "img_url")
+    private String imgUrl;
 
     // @ManyToOne um ninja tem uma unica missão
     @ManyToOne
