adf18a1cad8dd939202cb1946b1323d12d5aa004
eduardonk9999 <thiago.novak7@gmail.com>
2025-01-30 07:55:14 -0300

    controller CRUD

--- src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -1,6 +1,40 @@
 package dev.duzera.CadastroDeNinjas.Ninjas.Controller;
 
-// Controller 
+// Controller
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class NinjaController {
 
+    // Adc Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mosrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja";
+    }
+
+    // Mostrar ninja por ID (CREATE)
+    @GetMapping("/todosId")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar Ninja por ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja pro ID";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarId")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por ID";
+    }
 }
