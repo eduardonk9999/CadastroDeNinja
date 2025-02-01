2347e324a7fcebea6fdb401113f6d1a6928a6757
eduardonk9999 <thiago.novak7@gmail.com>
2025-02-01 16:17:38 -0300

    missoes controller

---
+++ src/main/java/dev/duzera/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,31 @@
+package dev.duzera.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missões listadas com sucesso!!!";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso!!!";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso!!!";
+    }
+
+
+
+}
