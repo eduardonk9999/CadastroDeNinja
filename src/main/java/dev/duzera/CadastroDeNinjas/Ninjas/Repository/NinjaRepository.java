93ec85e5553d817373ead116fd07d0b2e2b4fef1
Eduardo Silva <thiago.novak7@gmail.com>
2025-02-13 13:45:29 -0300

    optional

--- src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Repository/NinjaRepository.java
+++ src/main/java/dev/duzera/CadastroDeNinjas/Ninjas/Repository/NinjaRepository.java
@@ -3,5 +3,5 @@ package dev.duzera.CadastroDeNinjas.Ninjas.Repository;
 import dev.duzera.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+public interface NinjaRepositor <NinjaModel, Long> {
 }
